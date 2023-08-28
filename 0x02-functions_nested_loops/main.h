293f76d33e5513a7f3cfaa6640046e0041a1b7ca
mobakr98 <mobakr98@gmail.com>
2023-08-28 04:31:44 -0700

    Sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -32,4 +32,13 @@ int _islower(int c);
  * Return: 1 or 0
  */
 int _isalpha(int c);
+
+/**
+ * print_sign - the main function
+ *
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n);
 #endif
