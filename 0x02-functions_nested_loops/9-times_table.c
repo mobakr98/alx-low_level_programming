00aadec036e5c0a9fb6b4d5b37e43b8f25782f2d
mobakr98 <mobakr98@gmail.com>
2023-08-28 08:48:46 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,8 @@ for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
-multi = x * y;
-return (multi);
+z = x * y;
+return (z);
 _putchar ('  ');
 }
 _putchar ('n');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,72 +1,20 @@
 #ifndef _ALX_H
 #define _ALX_H
-/**
- * _putchar - writes the character c to std
- * @c: parameter to compare
- * Return: 1 or 0
- */
+
 int _putchar(char c);
 
-/**
- * print_alphabet - function to print alphabet
- *
- */
 void print_alphabet(void);
 
-/**
- * print_alphabet_x10 - prints alphabet 10 times
- *
- */
-void print_alphabet_x10(void);
-
-/**
- * _islower - prints if c is lower case
- * @c: character to use
- * Return: 1 or 0
- */
 int _islower(int c);
 
-/**
- * _isalpha - prints if character is alphabet
- * @c: character to use
- * Return: 1 or 0
- */
 int _isalpha(int c);
 
-/**
- * print_sign - the main function
- *
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
- */
 int print_sign(int n);
 
-/**
- * _abs - function to compute the absolute value of an integer
- *
- * @int: integer
- * Return: abs value of an integer
- */
-int _abs(int);
-
-/**
- * print_last_last - prints digit last
- * 
- * @int: integer to input
- * Return: last digit of integer
- */
 int print_last_digit(int);
 
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
- *
- */
 void jack_bauer(void);
 
-/**
- * times_table -  prints the 9 times table, starting with 0
- *
- */
 void times_table(void);
+
 #endif
