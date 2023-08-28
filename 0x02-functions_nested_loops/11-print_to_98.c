c25f5fb07c85b6063a8036d8f63536fd2f2bd1db
mobakr98 <mobakr98@gmail.com>
2023-08-28 10:17:19 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * Return: Always 0
+ *
+ * @n: number to check
+ *
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d, ", n);
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d, ", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
