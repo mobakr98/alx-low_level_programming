cfce4bf84a51a165e06431f4371047209bbb599d
mobakr98 <mobakr98@gmail.com>
2023-08-29 06:43:53 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,7 +5,7 @@
  * print_numbers - function that prints the numbers, from 0 to 9
  * followed by a new line
  *
- * Return: the result of multiplication
+ * Return: void
  *
  */
 
@@ -15,7 +15,7 @@ char c;
 
 for (c = '0'; c <= '9'; c++)
 {
-_putchar (c);
+putchar (c);
 }
-_putchar ('\n');
+putchar ('\n');
 }
