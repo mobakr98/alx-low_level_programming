78cae45cd93184a61b1fcf5778c1d806b1a3855f
mobakr98 <mobakr98@gmail.com>
2023-08-29 05:59:19 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isupper - function that checks for uppercase character
+ *
+ * @c: character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ *
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+_putchar ('1');
+}
+_putchar ('0');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+C - More Functions, More Nested Loops
+
+This repository contains a set of C programs that delve into the use of advanced concepts
+such as functions and nested loops in the C programming language.
