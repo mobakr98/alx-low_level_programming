36ad47e59b1667d503b201305c3a9ae8b374b2fb
mobakr98 <mobakr98@gmail.com>
2023-08-29 13:33:06 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ *
+ * @n: number of times the character _ should be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+_putchar (n);
+}
