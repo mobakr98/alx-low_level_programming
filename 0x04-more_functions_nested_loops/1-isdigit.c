11aa4a437904bdfc166745f74540ebe2efca6d74
mobakr98 <mobakr98@gmail.com>
2023-08-29 06:04:35 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,13 +6,13 @@
  *
  * @c: character to check
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is digit, 0 otherwise
  *
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= 0 && c <= 9)
 {
 return (1);
 }
