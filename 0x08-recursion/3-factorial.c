67da5e4faee6f11f7454a6fe9fbad4108662059b
mobakr98 <mobakr98@gmail.com>
2023-09-03 08:58:33 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -13,19 +13,19 @@ int factorial(int n)
 {
 int fact;
 
-fact = 1;
+fact = n;
 if (n < 0)
 {
 return (-1);
 }
-if (n == 1)
+else if (n == 1)
 {
 return (1);
 }
 else
 {
-fact = fact *  n;
 factorial(n - 1);
+fact = fact *  n;
 }
 return (fact);
 }
